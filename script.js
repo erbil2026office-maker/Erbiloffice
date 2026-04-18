bc59c6d5746f3003320b7c86c32effe5e6b13209
erbil2026office-maker <erbil2026office@gmail.com>
2026-04-18 14:45:22 +0300

    update index

--- script.js
+++ script.js
@@ -181,18 +181,19 @@ document.addEventListener('click', function(event) {
 
 function togglePassword() {
     const passInput = document.getElementById('password');
-    const eyeIconSpan = document.querySelector('.eye-icon'); // Select the span element
-    const eyeIconSvg = eyeIconSpan ? eyeIconSpan.querySelector('svg') : null; // Select the SVG inside the span
+    const icon = document.querySelector('.eye-icon i');
 
-    if (passInput && eyeIconSpan && eyeIconSvg) {
+    if (passInput && icon) {
+        // زیادکردنی ئەنیمەیشنێکی بچووک لەکاتی کلیک
+        icon.classList.add('eye-icon-anim');
+        setTimeout(() => icon.classList.remove('eye-icon-anim'), 300);
+
         if (passInput.type === 'password') {
             passInput.type = 'text';
-            eyeIconSvg.innerHTML = '<path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"/><path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-4.143 1.12l.57.57a3.501 3.501 0 0 1-4.743-4.743l.57.57a2.5 2.5 0 0 0 3.603 3.603z"/><path d="M12.457 13.359c-1.518 1.708-3.237 2.641-4.457 2.641-5 0-8-5.5-8-5.5a7.028 7.028 0 0 1 1.053-1.638L.15 7.15a.5.5 0 0 1 .708-.708l12.303 12.303a.5.5 0 0 1-.708.708l-1.457-1.457z"/>';
-            eyeIconSpan.classList.add('eye-slash-active'); // Add class for specific color
+            icon.className = 'fas fa-eye-slash active';
         } else {
             passInput.type = 'password';
-            eyeIconSvg.innerHTML = '<path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/><path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>';
-            eyeIconSpan.classList.remove('eye-slash-active'); // Remove class
+            icon.className = 'fas fa-eye';
         }
     }
 }
